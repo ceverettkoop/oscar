c6fcc1f4a21f742df5b557cd2947cd80b578e9d0
Sean McBride <brennsto@gmail.com>
2023-02-03 22:35:21 -0800

    some build order additions

--- BuildOrder.cpp
+++ BuildOrder.cpp
@@ -1,5 +1,6 @@
 #include <BuildOrder.h>
 #include <BWAPI.h>
+#include <fstream>
 
 
 //constructor
@@ -13,19 +14,61 @@ bool InitialBuildOrder::isFinished(int supplyCount){
 
 }
 
-BWAPI::UnitTypes::Enum::Enum InitialBuildOrder::nextStep(int supplyCount, int* targetCount){
+BWAPI::UnitType InitialBuildOrder::nextStep(int supplyCount, int* targetCount){
 
-    for (size_t i = 0; i < ibo_supplyCount.back(); i++){
+    for (size_t i = 0; i < ibo_supplyCount.size(); i++){
         if (supplyCount >= ibo_supplyCount[i]){
-            *targetCount = ibo_supplyCount[i];
-            return ibo_unitType[i];
+            if(supplyCount == ibo_supplyCount[i]){
+                *targetCount = ibo_supplyCount[i];
+                return ibo_unitType[i];
+            }else{
+                *targetCount = ibo_supplyCount[i];
+                return BWAPI::Broodwar->self()->getRace().getWorker();
+            }
+            
         }
     }
-    
 
 }
 
-
+//take a file of integers seperated by space or newline of the format:
+//supplycount unittype \n supplycount unittype etc etc
+//returns -1 on failure otherwise returns # of steps found in build order
 int InitialBuildOrder::load_ibo(std::string* path){
+
+    std::ifstream infile (*path);
+    std::string instring;
+    char inchar;
+    int inint;
+    bool isCount = true;
+    int i = 0;
+    
+    if(infile.is_open()){
+        while (infile.good()){
+            inchar = infile.get();
+            if( inchar != ' ' && inchar != '\n'){
+                instring += inchar;
+            }else{ //presumably we have an int
+                inint = std::stoi(instring, NULL);
+                if(isCount){
+                    ibo_supplyCount[i] = inint;
+                    isCount = false;
+                    instring.clear();
+                }else{
+                    ibo_unitType[i] = BWAPI::UnitType(inint);
+                    isCount = true;
+                    i++;
+                    instring.clear();
+                }
+            }
+        }
+        
+    }else {
+        fprintf(stderr, "Failed to load input file\n");
+        return -1;
+    }
+
+    stepCount = i;
+    return i;
 
 }
--- BuildOrder.h
+++ BuildOrder.h
@@ -8,12 +8,31 @@
 class InitialBuildOrder{
 	//just stores what to build at drone count x
 	std::vector<int> ibo_supplyCount; //requisite count to build unit or struct at i
-	std::vector<BWAPI::UnitTypes::Enum::Enum> ibo_unitType; //type of unit to build
-
+	std::vector<BWAPI::UnitType> ibo_unitType; //type of unit to build
+	int stepCount;
 public:
 	InitialBuildOrder(); //constructor
 
 	bool isFinished(int supplyCount);
-	BWAPI::UnitTypes::Enum::Enum InitialBuildOrder::nextStep(int supplyCount, int* targetCount);
+	BWAPI::UnitType nextStep(int supplyCount, int* targetCount);
 	int load_ibo(std::string* path);
 };
+
+
+
+/*
+store next thing to build in static variable
+
+every frame
+if else tree:
+are we on initial build order (static bool)
+	if yes nextStep
+		if supplyCount = targetCount
+			next = unit type
+		else build probe
+else (not on initial build order)
+	some decision function????
+
+
+
+*/
