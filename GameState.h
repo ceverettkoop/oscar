628d8f8888227d7e26ca10d6b41595720ac5e5d6
ceverettkoop <79296565+ceverettkoop@users.noreply.github.com>
2023-03-03 10:50:17 -0800

    tiny add

--- GameState.h
+++ GameState.h
@@ -1,19 +1,23 @@
 #pragma once
 #include <BWAPI.h>
 #include "BWEM/bwem.h"
+#include <map>
 
 class OscarMap;
 
 typedef struct baseEcon{
     int onGas;
     int onMin;
+    const BWEM::Base* basePtr;
 
 };
+
 
 class GameState{
     
 public:
     bool isScouting = false;
     OscarMap* mapPtr;
+    std::map<baseEcon, int> workerTotals;
     
 };
